12a87a757ee106fa9d258d4c081478897715d57c
candy197 <candy.jacob1997@gmail.com>
2024-08-27 12:19:29 +0530

    FirstCommit

---
+++ Basic_In_Out.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a, b,sum;
+
+    cout<<"Enter a number: "<<endl;
+    cin>>a;
+    cout<<"enter a second number"<<endl;
+    cin>>b;
+    sum = a+b;
+
+    cout<<"sum of the number: "<<sum<<endl;
+  
+}
