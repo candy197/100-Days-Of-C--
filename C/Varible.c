3e77c22e42c0f85892a0787b2999f2c7b14794c6
candy197 <candy.jacob1997@gmail.com>
2024-10-14 17:56:12 +0530

    commits

--- C/Varible.c
+++ C/Varible.c
@@ -1,8 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
+int main()
+{
+    int a = 1;
+    float b = 5.3;
+    char c = 'a';
 
-int main(){
-    printf("Hellow world!");
+    printf("This string %c and this is int %d, and this is float %f \n", c, a, b);
 
+    printf("Hellow world! \n");
     return 0;
 }
