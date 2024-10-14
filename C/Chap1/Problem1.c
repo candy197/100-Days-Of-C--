aadb6130c01419346a56d706bc25ebe2257fc256
candy197 <candy.jacob1997@gmail.com>
2024-10-14 18:23:42 +0530

    Day 1 of C

---
+++ C/Chap1/Problem1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int lenght,breadth;
+
+    //Find a Area of rectangle
+    printf("Enter Length\n");
+    scanf("%d",&lenght);
+    printf("Enter Breath of  Rectangle \n");
+    scanf("%d",&breadth);
+    printf("The area of  rectangle is %d\n",lenght*breadth);
+         
+
+    return 0;
+
+}       
