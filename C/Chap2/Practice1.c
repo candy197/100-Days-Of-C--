49a4e5f04224395c28e48ddb5c2b0daea9e144ae
candy197 <candy.jacob1997@gmail.com>
2024-10-15 14:27:23 +0530

    day 2

--- C/Chap2/Practice1.c
+++ C/Chap2/Practice1.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main(){
+    //explain the 
+    int x=2,y=3,z=3,k=1;
 
-     
+    float e = 3*x/y - z+k;
+    printf("The value of e is %f \n",e);     
     return 0;
 
 }
