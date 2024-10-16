7d9338fef00343963f85013164ee12bbbca8c4da
candy197 <candy.jacob1997@gmail.com>
2024-10-16 12:51:17 +0530

    Day 4 files

---
+++ C/Chap4/ForLoop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n = 6;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Value of i is %d \n", i);
+    }
+
+    return 0;
+}
---
+++ C/Chap4/LoopPractice.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // while loop example
+    int i = 0;
+    while (i < 4)
+    {
+        printf("While loop is working at iteration: %d \n", i);
+        i++;
+    }
+
+    return 0;
+}
