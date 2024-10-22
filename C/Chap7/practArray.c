dee69d1cace0fdfbfff23ea6ce24c2e6917e688b
candy197 <candy.jacob1997@gmail.com>
2024-10-22 20:06:31 +0530

    day 7 C

--- C/Chap7/practArray.c
+++ C/Chap7/practArray.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-       
+    int count;
+    printf("Enter How many Subject you want to enter: \n");
+    scanf("%d", &count);
+    int marks[count];
+    for (int i = 0; i < count; i++)
+    {
+        printf("Enter a Mark for Subject %d: \n", i);
+        scanf("%d", &marks[i]);
+    }
+
+    for(int i = 0;i<count;i++){
+        printf("The value of index %d is %d \n",i,marks[i]);
+    }
+
     return 0;
 }
